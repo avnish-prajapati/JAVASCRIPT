c6c22a2aabbbd4aea9876d9ac5e4db70757cbe70
Avnish Prajapati <avnishprajapati664@gmail.com>
2026-04-22 22:04:17 +0530

    Update cart.js

--- JAVASCRIPT REVISION/DAY - 1/Ecart list/cart.js
+++ JAVASCRIPT REVISION/DAY - 1/Ecart list/cart.js
@@ -6,22 +6,20 @@ const totalPrice = document.getElementById("total-price");
 
 
 
-function removecarts(i)
-{
+function removecarts(i) {
     let arr = JSON.parse(localStorage.getItem("carts"));
     arr.splice(i, 1);
-    localStorage.setItem("carts",JSON.stringify(arr));
+    localStorage.setItem("carts", JSON.stringify(arr));
     displaycartproducts();
 }
 
-function displaycartproducts()
-{   
+function displaycartproducts() {
     let Total = 0;
     const carts = JSON.parse(localStorage.getItem("carts")) || [];
     cartDivison.innerHTML = "";
     carts.forEach((element, i) => {
         Total = Total + (element.price * 93);
-      const div = document.createElement("div");
+        const div = document.createElement("div");
         div.innerHTML = `<div class="card mb-3" style="max-width: 540px;">
                 <div class="row g-0">
                     <div class="col-md-4">
@@ -31,15 +29,15 @@ function displaycartproducts()
                         <div class="card-body">
                             <h5 class="card-title">${element.title}</h5>
                             <h5 class="card-price">Price ₹${(element.price * 93).toFixed(2)}/-</h5>
-                            <h5 class="card-text">Qty ${element.Qty}</h5>
+                            <h5 class="card-text">Qty ${element.qty}</h5>
                             <button onclick="removecarts(${i})" class="btn btn-outline-danger">Remove</button>
                         </div>
                     </div>
                 </div>
             </div>`;
-            cartDivison.appendChild(div)
-  });
-  totalPrice.textContent ="Total : ₹" +(Total.toFixed(2)) + "/-"
+        cartDivison.appendChild(div)
+    });
+    totalPrice.textContent = "Total : ₹" + (Total.toFixed(2)) + "/-"
 }
 
 displaycartproducts();
